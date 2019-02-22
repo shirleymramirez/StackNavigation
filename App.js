8f1fb48e9945a8a4b7872704829c1423180a73b3
shirley <shirley.mramirez@yahoo.com>
2019-02-21 22:31:17 -0700

    Added Header

--- App.js
+++ App.js
@@ -8,17 +8,36 @@
  */
 
 import React, { Component } from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Image, Button } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
+
+class LogoTitle extends React.Component {
+  render() {
+    return ( 
+      <Image 
+        source = { require('./src/images/cake.jpg')}
+        style = {{ width: 30, height: 30}}
+      />
+    );
+  }
+}
+
 class HomeScreen extends Component {
+  static navigationOptions = {
+    headerTitle: < LogoTitle />,
+  };
+
   render() {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text>Home Screen</Text>
         <Button
           title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details')}
+          onPress={() => this.props.navigation.navigate('Details', {
+            itemId: 86,
+            otherParam: 'anything you want here!',
+          })}
         />
 
       </View>
@@ -27,13 +46,38 @@ class HomeScreen extends Component {
 }
 
 class DetailsScreen extends Component {
+  static navigationOptions = ({ navigation, navigationOptions }) => {
+    const { params } = navigation.state;
+
+    return {
+      title: params ? params.otherParam : 'A Nested Details Screen',
+      headerStyle: {
+        backgroundColor: navigationOptions.headerTintColor,
+      },
+      headerTintColor: navigationOptions.headerStyle.backgroundColor,
+    };
+  };
+
   render() {
-    return (
+    const { navigation } = this.props;
+    const itemId = navigation.getParam('itemId', 'NO-ID');
+    const otherParam = navigation.getParam('other Param', 'some default value');
+    
+    return ( 
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text>Details Screen</Text>
+        <Text>itemId: {JSON.stringify(itemId)}</Text>
+        <Text>otherParam: {JSON.stringify(otherParam)}</Text>
         <Button
           title="Go to Details... again"
-          onPress={() => this.props.navigation.push('Details')}
+          onPress={() => this.props.navigation.push('Details', {
+            itemId: Math.floor(Math.random() * 100),
+          })}
+        />
+        <Button
+          title="Update the title"
+          onPress={() =>
+            this.props.navigation.setParams({ otherParam: 'Updated!' })}
         />
         <Button
           title="Go to Home"
@@ -43,11 +87,11 @@ class DetailsScreen extends Component {
           title="Go back"
           onPress={() => this.props.navigation.goBack()}
         />
-
       </View>
     );
   }
 }
+
 
 const AppNavigator = createStackNavigator(
   {
@@ -55,9 +99,19 @@ const AppNavigator = createStackNavigator(
     Details: DetailsScreen
   },
   {
-    initialRouteName: "Home"
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#f4511e',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
   }
 );
+
 
 const AppContainer = createAppContainer(AppNavigator);
 
